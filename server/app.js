29127d48b68b1eca8052ed66a601d8a350b209f2
TodV <d2tod12@gmail.com>
2025-11-30 17:16:23 -0600

    cambios de render

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = 4000;
 
 // Middleware
-app.use(cors({origin: process.env.CORS_ORIGIN,credentials: true}));
+app.use(cors({origin: "*", methods: ["GET", "POST", "PUT", "DELETE"]}));
 app.use(express.json());
 
 // Montar rutas
